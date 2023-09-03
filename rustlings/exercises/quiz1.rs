c89c79bcdf7696d98221270ce1e879db81d70eda
init1 <28729137+0xinit@users.noreply.github.com>
2023-09-03 02:08:54 -0500

    finished quiz1

--- rustlings/exercises/quiz1.rs
+++ rustlings/exercises/quiz1.rs
@@ -10,11 +10,15 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
-
+fn calculate_price_of_apples(quantity: u32) -> u32 {
+    if quantity > 40 {
+        quantity
+    } else {
+        quantity * 2
+    }
+    // can be written as: if (quantity > 40) { quantity } else { quantity * 2 }
+}
 // Don't modify this function!
 #[test]
 fn verify_test() {
